ab58e30c2e08a05ddded84282bbbd7f066e10c79
F3licia <romeofelicia@gmail.com>
2025-11-14 22:01:45 +0100

    chore: disable dark mode

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  darkMode: false, //eventualmente cambiato con class
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
