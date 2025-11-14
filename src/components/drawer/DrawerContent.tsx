f93067ddcd9f8ec8ed09ce444085cdb3d1805745
F3licia <romeofelicia@gmail.com>
2025-11-14 21:06:46 +0100

    chore: rollback imports

--- src/components/drawer/DrawerContent.tsx
+++ src/components/drawer/DrawerContent.tsx
@@ -1,6 +1,4 @@
 import { BsAsterisk } from "react-icons/bs";
-import frame from './images/frame.png';
-import codebar from './images/codebar.png';
 
 export function DrawerContent() {
 
@@ -18,7 +16,7 @@ export function DrawerContent() {
 
   const description = <div className="self-end text-black text-xs text-justify w-[200px]">
     <div className="frame-wrapper mb-1">
-      <img width={200} src={codebar} />
+      <img width={200} src="/images/codebar.png" />
     </div>
     Single-page application designed to practice global state management using the Redux library. The Kanban board states are managed globally, while data persistence is handled through the browser's localStorage
   </div>;
@@ -30,7 +28,7 @@ export function DrawerContent() {
       {description}
 
       <div className="frame-wrapper absolute bottom-4 md:bottom-9 ">
-        <img height={140} width={140} src={frame} />
+        <img height={140} width={140} src="/images/frame.png" />
       </div>
     </div>
   );
